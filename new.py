fe477f52a47e4e8bc26ba4047265ca07552f743f
Yashveeryashu <yashveerlangyan@gmail.com>
2025-02-06 15:03:05 +0530

    Initial2 commit

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 from langchain_openai import AzureChatOpenAI
 from langchain.agents.agent_types import AgentType
